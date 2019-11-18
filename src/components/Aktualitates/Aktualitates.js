8efd71dac29a180e49d49c6ba94953d9a0cedf03
Klāvs Spruģevics <klavs.sprugevics@gmail.com>
2019-11-18 15:39:00 +0200

    Veiksmīgi izveidots savienojums ar savu API. Atgūtas aktualitātes no sava API un izvadītas.

--- src/components/Aktualitates/Aktualitates.js
+++ src/components/Aktualitates/Aktualitates.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import "./Aktualitates.scss";
+import axios from 'axios';
 
 
 class Aktualitates extends Component{
@@ -9,20 +10,52 @@ class Aktualitates extends Component{
         super();
         this.state = 
         {
-
+            isLoaded: false,
+            response: null
         }
+    }
+
+    componentDidMount()
+    {
+        this.getData();
+    }
+
+    getData()
+    {
+        // Atgūstam datus no sava API
+        axios.get("http://localhost:5000/api/aktualitates")
+        .then(res =>
+        {
+            // console.log(res.data);
+            this.setState({response: res.data, isLoaded: true});
+        });
+        
     }
 
     render()
     {
-        return(
-        <div>
-            <h1>Jaunākais studentu dzīvē!</h1>
-            
-            
-        </div>
-        );
-
+        
+        if(!this.state.isLoaded)
+        {
+            return(<h1>Loading...</h1>)
+        }
+        else
+        {
+            return(
+            <div>
+                <h1>Jaunākais studentu dzīvē!</h1>
+                <ul>
+                    {this.state.response.aktualitates.map(posts => 
+                    <div key={posts.id}>
+                        <span className="aktNosaukums">{posts.datat.nosaukums}</span><br/>
+                        <span className ="aktApraksts">{posts.datat.apraksts}</span><br/>
+                        <span className="aktAutors">{posts.datat.autors}</span><br/>
+                        <hr/>
+                    </div>)}
+                </ul>
+            </div>
+            );
+        }   
     }
 }
 
