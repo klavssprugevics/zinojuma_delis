e4aa7f85e9198652e47ba54c4f9e21f0115fe122
Klāvs Spruģevics <klavs.sprugevics@gmail.com>
2019-11-20 07:15:26 +0200

    Vienkāršas lauku validācijas 'pievienot' un 'labot' lapām

--- src/components/Aktualitates/AktualitatesLabot.js
+++ src/components/Aktualitates/AktualitatesLabot.js
@@ -16,6 +16,8 @@ class AktualitatesLabot extends Component{
             autors: '',
             submitPressed: false,
             deleted: false,
+            errors: {},
+            isFormCorrect: true,
         }
         this.ParseInput = this.ParseInput.bind(this);
         this.DeletePost = this.DeletePost.bind(this);
@@ -60,18 +62,56 @@ class AktualitatesLabot extends Component{
     {
         // Pārbaude -> nevar būt mazāk par 5 simboliem un saturēt tikai no simboliem.
 
+        const nosaukums = this.state.nosaukums;
+        const apraksts = this.state.apraksts;
+        const autors  = this.state.autors;
+        const onlyLettersRegex = /^[A-Za-z āēīūģķļņčšž]+$/;
+
+        let isFormCorrect = true;
+        let errors = {};
+
         const data = {
-            "nosaukums": this.state.nosaukums,
-            "apraksts": this.state.apraksts,
-            "autors": this.state.autors
+            "nosaukums": nosaukums,
+            "apraksts": apraksts,
+            "autors": autors
         };
 
-        axios.put("http://localhost:5000/api/aktualitates/" + this.props.match.params.id, data)
-        .then(response => 
+        if(nosaukums === "")
         {
-            this.setState({submitPressed: true});
+            isFormCorrect = false;
+            errors["nosaukums"] = "'Nosaukums' nevar būt tukšs.";
+        }
+        if(apraksts === "")
+        {
+            isFormCorrect = false;
+            errors["apraksts"] = "'Apraksts' nevar būt tukšs.";
+        }
+        if(!onlyLettersRegex.test(autors))
+        {
+            isFormCorrect = false;
+            errors["autors"] = "'Autors' var sastāvēt tikai no burtiem.";
+        }
+        if(autors==="")
+        {
+            isFormCorrect = false;
+            errors["autors"] = "'Autors' nevar būt tukšs.";
+        }
+
+        if(isFormCorrect)
+        {
+            axios.put("http://localhost:5000/api/aktualitates/" + this.props.match.params.id, data)
+            .then(response => 
+            {
+                this.setState({submitPressed: true});
+            })
+            .catch(error => console.log(error));
+        }
+        this.setState({
+            isFormCorrect,
+            errors
         })
-        .catch(error => console.log(error));
+
+
 
 
     }
@@ -109,8 +149,13 @@ class AktualitatesLabot extends Component{
                         <span className="editAktVirsraksts">Labošana...</span>
                         
                         <input className="textInput" name="nosaukums" type="text" placeholder="Nosaukums" value={this.state.nosaukums} onChange={evt => this.updateInput(evt)} /><br/>
+                        <div className="errorMsg">{this.state.errors.nosaukums}</div>
+                        
                         <textarea className="textareaInput" name="apraksts" type="text" placeholder="Apraksts" value={this.state.apraksts}  onChange={evt => this.updateInput(evt)} /><br/>
+                        <div className="errorMsg">{this.state.errors.apraksts}</div>
+  
                         <input className="textInput" name="autors" type="text" placeholder="Autors" value={this.state.autors} onChange={evt => this.updateInput(evt)} /><br/>
+                        <div className="errorMsg">{this.state.errors.autors}</div>
 
                         <button className="buttonLabot" type="button" onClick={this.ParseInput}>Labot!</button> 
                         <button className="buttonDzest" type="button" onClick={this.DeletePost}>Dzēst!</button> 
--- src/components/Aktualitates/AktualitatesPievienot.js
+++ src/components/Aktualitates/AktualitatesPievienot.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import './AktualitatesPievienot.scss';
 import axios from 'axios';
 import {Redirect} from 'react-router-dom';
+import { tsConstructSignatureDeclaration } from '@babel/types';
 
 
 class AktualitatesPievienot extends Component{
@@ -15,29 +16,65 @@ class AktualitatesPievienot extends Component{
             apraksts: '',
             autors: '',
             submitPressed: false,
+            errors: {},
+            isFormCorrect: true,
         }
         this.ParseInput = this.ParseInput.bind(this);
     }
 
     ParseInput()
     {
-        // Pārbaude -> nevar būt mazāk par 5 simboliem un saturēt tikai no simboliem.
         
+        const nosaukums = this.state.nosaukums;
+        const apraksts = this.state.apraksts;
+        const autors  = this.state.autors;
+        const onlyLettersRegex = /^[A-Za-z āēīūģķļņčšž]+$/;
+
+        let isFormCorrect = true;
+        let errors = {};
 
         const data = {
-            "nosaukums": this.state.nosaukums,
-            "apraksts": this.state.apraksts,
-            "autors": this.state.autors
+            "nosaukums": nosaukums,
+            "apraksts": apraksts,
+            "autors": autors
         };
 
-        axios.post("http://localhost:5000/api/aktualitates", data)
-        .then(response => 
+        if(nosaukums === "")
         {
-            console.log(response)
-            this.setState({submitPressed: true});
-        })
-        .catch(error => console.log(error));
+            isFormCorrect = false;
+            errors["nosaukums"] = "'Nosaukums' nevar būt tukšs.";
+        }
+        if(apraksts === "")
+        {
+            isFormCorrect = false;
+            errors["apraksts"] = "'Apraksts' nevar būt tukšs.";
+        }
+        if(!onlyLettersRegex.test(autors))
+        {
+            isFormCorrect = false;
+            errors["autors"] = "'Autors' var sastāvēt tikai no burtiem.";
+        }
+        if(autors==="")
+        {
+            isFormCorrect = false;
+            errors["autors"] = "'Autors' nevar būt tukšs.";
+        }
 
+
+        if(isFormCorrect)
+        {
+            axios.post("http://localhost:5000/api/aktualitates", data)
+            .then(response => 
+            {
+                console.log(response)
+                this.setState({submitPressed: true});
+            })
+            .catch(error => console.log(error));
+        }
+        this.setState({
+            isFormCorrect,
+            errors
+        })
 
 
 
@@ -67,9 +104,14 @@ class AktualitatesPievienot extends Component{
                     <span className="pievienotAktualitatiVirsraksts">Pievieno jaunu aktualitāti!</span>
                     
                     <input className="textInput" name="nosaukums" type="text" placeholder="Nosaukums" value={this.state.nosaukums} onChange={evt => this.updateInput(evt)} /><br/>
+                    <div className="errorMsg">{this.state.errors.nosaukums}</div>
+
                     <textarea className="textareaInput" name="apraksts" type="text" placeholder="Apraksts" value={this.state.apraksts}  onChange={evt => this.updateInput(evt)} /><br/>
+                    <div className="errorMsg">{this.state.errors.apraksts}</div>
+                    
                     <input className="textInput" name="autors" type="text" placeholder="Autors" value={this.state.autors} onChange={evt => this.updateInput(evt)} /><br/>
-    
+                    <div className="errorMsg">{this.state.errors.autors}</div>
+
                     <button className="buttonPievienot" type="button" onClick={this.ParseInput}>Publicēt!</button> 
                 </div>
             );
