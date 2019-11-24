616b86a33268ad4fe71b3a42ad25bb9d2263824c
Klāvs Spruģevics <klavs.sprugevics@gmail.com>
2019-11-24 19:50:28 +0200

    Pievienota meklēšans funkcionalitāte

--- src/components/shared-functions/ViewInfo.js
+++ src/components/shared-functions/ViewInfo.js
@@ -24,11 +24,16 @@ class ViewInfo extends Component{
         super(props);
         this.state = 
         {
-            isLoaded: false,
+            originalResponse: null,
             response: null,
+            keyword: "",
             delete: false,
+            isSearchPressed: false,
+            isLoaded: false
         }
         this.DeletePost = this.DeletePost.bind(this);
+        this.SearchPost = this.SearchPost.bind(this);
+        this.ResetSearch = this.ResetSearch.bind(this);
     }
 
     componentDidMount()
@@ -41,8 +46,15 @@ class ViewInfo extends Component{
         axios.get("http://localhost:5000/api/" + this.props.urlParams)
         .then(res =>
         {
-            this.setState({response: res.data, isLoaded: true});
-        });
+            this.setState({
+                originalResponse: res.data,
+                response: res.data, 
+                isLoaded: true,
+            }, () =>
+            {
+                this.SearchPost();
+            });
+        })
     }
 
     DeletePost(aktID)
@@ -53,6 +65,62 @@ class ViewInfo extends Component{
             this.setState({isLoaded:false});
             this.componentDidMount();
         });
+    }
+
+    updateInput(event)
+    {
+        const target = event.target;
+        const name = target.name;
+        this.setState({
+            [name]: event.target.value
+          });
+    }
+
+    // Funkcija, kas realizē meklēšanu.
+    SearchPost()
+    {
+        const keyword = this.state.keyword;
+
+        // Pārbauda, vai search nav tukšs - nav jēgas meklēt.
+        if(keyword === "" || keyword.length < 3)
+        {
+            return;
+        }
+        
+        // Array, kas saturēs atrastos postus.
+        let newPosts = [];
+
+        // Atrodam visus postus, kas atbilst keywordam
+        newPosts = this.state.response[this.props.urlParams].filter(posts =>
+        {
+            return posts.datat[this.props.fieldNames[0]].toUpperCase().includes(this.state.keyword.toUpperCase())
+        });
+        
+        // Izveidojam jaunu response objektu, ar kuru aizvietot veco.
+        let newResponse = this.state.response;
+        newResponse[this.props.urlParams] = [];
+
+        // Pievienojam tam atrastos postus.
+        newPosts.forEach(post =>
+        {
+            newResponse[this.props.urlParams].push(post);
+        });
+
+        // Apmainām response.
+        this.setState({
+            response: newResponse,
+            isSearchPressed: true
+        })
+    }
+
+    // Atjaunojam lapu sākotnējā stadijā.
+    ResetSearch()
+    {
+        this.setState({
+            keyword: "",
+            isSearchPressed: false
+        })
+        this.getData();
     }
 
     render()
@@ -68,6 +136,11 @@ class ViewInfo extends Component{
                 <div className="virsrakstsDiv">
                     <h1 className="virsraksts">{this.props.customMessage}</h1>
                     <NavLink className="pievienotLink" to="/addinfo">Pievienot info...</NavLink>
+                </div>
+                <div className="searchSection">
+                    <input className="textInput" name="keyword" type="text" placeholder="Meklēt (vismaz 3 simboli)" value={this.state.keyword} onChange={evt => this.updateInput(evt)} /><br/>
+                    <button className="buttonPievienot" type="button" onClick={() => this.getData()}>Meklēt!</button>
+                    {this.state.isSearchPressed === true && <button className="cancelSearch" type="button" onClick={() => this.ResetSearch()}><i className="fa fa-times-circle fa-2x"></i></button>}     
                 </div>
                 <ul>
                     {/* Tiek iterēts cauri visiem iegūtajiem objektiem no response,
